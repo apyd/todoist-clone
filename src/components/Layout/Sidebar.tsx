52056181eed71dea4f57d0570663440e07ae9af9
apyd <arturpydych@gmail.com>
2022-02-23 18:14:28 +0100

    Layout components - Header, sidebar and content

---
+++ src/components/Layout/Sidebar.tsx
@@ -0,0 +1,40 @@
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__categories">
+        <li className="sidebar__category">
+          <span className="category__icon">
+            <FaInbox />
+          </span>
+          <span className="category__title">Inbox</span>
+        </li>
+        <li className="sidebar__category">
+          <span className="category__icon">
+            <FaRegCalendar />
+          </span>
+          <span className="category__title">Today</span>
+        </li>
+        <li className="sidebar__category">
+          <span className="category__icon">
+            <FaRegCalendarAlt />
+          </span>
+          <span className="category__title">Next 7 days</span>
+        </li>
+      </ul>
+      <div className="sidebar__projects">
+        <header className="projects__header">
+          <FaChevronDown />
+          <h2 className="projects__heading">Projects</h2>
+        </header>
+      </div>
+      <ul className="projects__list">Projects will be here</ul>
+    </div>
+  );
+};
