de2dfb214782ecc6fa460df35996cc3081fc30ef
apyd <arturpydych@gmail.com>
2022-02-25 13:18:35 +0100

    Header improvements, search icon

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,44 +1,66 @@
+import { useState } from "react";
 import {
   FaStream,
   FaHome,
-  FaSearch,
   FaPizzaSlice,
   FaPlus,
   FaUserCircle,
+  FaSearch,
 } from "react-icons/fa";
 import "./styles.scss";
 
 export const Header = () => {
+  const [searchInput, setSearchInput] = useState<string>("");
+  const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const clearInput = () => {
+    setSearchInput("");
+  };
+
   return (
     <header className="header" data-testid="header">
       <nav className="nav">
         <ul className="nav__list">
-          <li className="sidebar__toggle">
+          <li className="nav__item">
             <FaStream className="icon" />
           </li>
-          <li className="home">
+          <li className="nav__item">
             <FaHome className="icon" />
           </li>
           <div className="search-box">
+            <span className="icon__wrapper">
+              <FaSearch className="icon icon--search" />
+            </span>
             <input
               type="text"
               className="search-box__input"
               id="search"
               name="search"
               placeholder="Search..."
+              onChange={(e) => searchHandler(e)}
+              value={searchInput}
             />
-            <FaSearch className="search-box__icon" />
+            {searchInput.length > 3 && (
+              <span
+                className="nav__item nav__item--positioned"
+                onClick={clearInput}
+              >
+                <FaPlus className="icon icon--clear" />
+              </span>
+            )}
             <label htmlFor="search" />
           </div>
         </ul>
-        <ul className="nav__list nav__list">
-          <li className="add-task">
+        <ul className="nav__list">
+          <li className="nav__item">
             <FaPlus className="icon" />
           </li>
-          <li className="toggle-theme">
+          <li className="nav__item">
             <FaPizzaSlice className="icon" />
           </li>
-          <li className="user-account">
+          <li className="nav__item">
             <FaUserCircle className="icon" />
           </li>
         </ul>
